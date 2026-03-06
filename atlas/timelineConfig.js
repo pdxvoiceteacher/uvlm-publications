333db3bb8c3a7af94219487086b4539c9fbfc723
Ultra Verba Lux Mentis <pdxvoiceteacher@gmail.com>
2026-03-06 15:46:42 -0800

    Add Sophia-audited reasoning-thread overlay ingestion and watchlist

---
+++ atlas/timelineConfig.js
@@ -0,0 +1,18 @@
+export const timelineConfig = {
+  defaultMode: 'static',
+  playbackSpeeds: {
+    '1x': 1000,
+    '2x': 500,
+    '5x': 200
+  },
+  animation: {
+    nodeFadeMs: 800,
+    edgeFadeMs: 500
+  },
+  conceptScaling: {
+    baseSize: 40,
+    sizeFactor: 8,
+    baseGlow: 18,
+    glowFactor: 6
+  }
+};
