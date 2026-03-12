3c8f11c667e8599254c33f5f6b039c5178f5c413
Ultra Verba Lux Mentis <pdxvoiceteacher@gmail.com>
2026-03-11 20:00:43 -0700

    Add recursive spiral operator formalization sketch

--- atlas/telRiverOverlay.js
+++ atlas/telRiverOverlay.js
@@ -1,16 +1,10 @@
-const RIVER_RESETTABLE_CLASSES = ['river-node', 'river-highlight'];
+export const riverResettableClasses = ['river-flowing'];
 
 export function applyRiverOverlay(cy, enabled = true) {
-  cy.nodes().removeClass(RIVER_RESETTABLE_CLASSES.join(' '));
+  cy.elements().removeClass(riverResettableClasses.join(' '));
   if (!enabled) return;
 
-  cy.nodes().forEach((node) => {
-    const riverFlow = node.data('riverFlow');
-    if (riverFlow) {
-      node.addClass('river-node river-highlight');
-      node.data('riverAdvisory', 'Advisory only, not authoritative: knowledge-river flow indicator.');
-    }
-  });
+  cy.elements().addClass('river-flowing');
 }
 
 export function bindRiverOverlayToggle(cy, toggleEl, reapply) {
@@ -19,8 +13,4 @@ export function bindRiverOverlayToggle(cy, toggleEl, reapply) {
     applyRiverOverlay(cy, Boolean(toggleEl.checked));
     if (typeof reapply === 'function') reapply();
   });
-}
-
-export function riverResettableClasses() {
-  return [...RIVER_RESETTABLE_CLASSES];
 }
